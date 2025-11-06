861d14ce912b5b16f16e71e72e9a8fc17edcba2c
huyox <nguyenhuy19052008@gmail.com>
2025-11-06 12:30:25 +0700

    Add files via upload
    [initial]

---
+++ script.js
@@ -0,0 +1,15 @@
+hljs.highlightAll();
+
+const year = document.getElementById("year");
+year.innerText = new Date().getFullYear().toString();
+
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.tab-content');
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(c => c.classList.remove('active'));
+        tab.classList.add('active');
+        document.getElementById(tab.dataset.tab).classList.add('active');
+    });
+});
